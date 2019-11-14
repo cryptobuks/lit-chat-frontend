0e3677290c0fa8dc3471da2a18fb4aa9c8d198f0
campbelllssoup <matjocampbell@gmail.com>
2019-11-13 20:00:41 -0800

    Makes changes to basic form for testing

---
+++ resources/js/index.js
@@ -0,0 +1,24 @@
+console.log("Now we're talking!!!\n\n");
+
+let loginForm = document.querySelector('#login');
+
+loginForm.addEventListener('submit', function getLoginInfo(e){
+  e.preventDefault();
+  // console.log('Username/email address:', e.target.identifier.value);
+  verifyAccount(e.target)
+})
+
+function verifyAccount(loginForm){
+  fetch(`${loginForm.action}`,{
+    method: `POST`,
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify({
+      identifier: loginForm.identifier.value
+    })
+  })
+    .then(res => res.json())
+    .then(console.log)
+}
